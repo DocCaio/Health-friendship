064cbc3b4a8c901a7947f49e9ec6b2ee35e555ba
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-09 13:07:16 -0300

    up

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,3 @@
-function clickMenu() {
-menu__nav.style.display ='block';
-if (menu__nav.style.display == 'block') {
-    menu__nav.style.display = 'none'
-} else {
-    menu__nav.style.display = 'block'
-}
-
-}
 
   //Email
   
---
+++ assets/menu.js
@@ -0,0 +1,10 @@
+function clickMenu() {
+    menu__nav.style.display ='block';
+    if (menu__nav.style.display == 'block') {
+        menu__nav.style.display = 'none'
+    } else {
+        menu__nav.style.display = 'block'
+    }
+    
+    }
+    
